0ebcdd9d024b0a2ddabb2fbefdaef1190c4c32a9
Yash-Raj-5424 <myash3499@gmail.com>
2026-03-27 23:20:15 +0530

    Initial commit

---
+++ src/main/java/com/url/TinyLynk/TinyLynkApplication.java
@@ -0,0 +1,13 @@
+package com.url.TinyLynk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyLynkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyLynkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/url/TinyLynk/TinyLynkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.url.TinyLynk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyLynkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
